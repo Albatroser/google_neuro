bb936587803f61337f84e601d37b99cc29ff35e8
Artem <wvwvkkkk@yandex.ru>
2018-08-23 11:35:00 +0700

    Запросы репостов в канал теперь согласовываются с администратором бота

--- app/main.py
+++ app/main.py
@@ -13,12 +13,17 @@ import random
 
 
 def write_log(message_id, username, input_text, output_text):
-	filename = 'logs/' + username + '.txt'
+	if username is not None:
+		filename = 'logs/' + username + '.txt'
+		log_file_hdr = strings.log_id + str(message_id) + '\n' + strings.log_username + username + '\n---\n\n'
+	else:
+		log_file_hdr = strings.log_id + str(message_id) + '\n---\n\n'
+		filename = 'logs/' + str(message_id) + '.txt'
 	if os.path.exists(filename):
 		file = codecs.open(filename, 'a', 'utf-8')
 	else:
 		file = codecs.open(filename, 'w', 'utf-8')
-		file.write(strings.log_id + str(message_id) + '\n' + strings.log_username + username + '\n---\n\n')
+		file.write(log_file_hdr)
 
 	file.write(strings.log_text_in + input_text + '\n\n' + strings.log_text_out + output_text + '\n---\n\n')
 	file.close()
@@ -26,11 +31,11 @@ def write_log(message_id, username, input_text, output_text):
 
 def remove_emoji(text):
 	emoji_pattern = re.compile("["
-								u"\U0001F600-\U0001F64F"
-								u"\U0001F300-\U0001F5FF"
-								u"\U0001F680-\U0001F6FF"
-								u"\U0001F1E0-\U0001F1FF"
-								"]+", flags=re.UNICODE)
+							   u"\U0001F600-\U0001F64F"
+							   u"\U0001F300-\U0001F5FF"
+							   u"\U0001F680-\U0001F6FF"
+							   u"\U0001F1E0-\U0001F1FF"
+							   "]+", flags=re.UNICODE)
 
 	return emoji_pattern.sub(r'', text)
 
@@ -70,10 +75,12 @@ def random_text(message):
 		write_log(message.chat.id, message.chat.username, rt, translation)
 	return translation
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--socks', action='store', dest='s', help=strings.socks5_help)
 parser.add_argument('-t', '--token', action='store', dest='t', help=strings.token_help)
 parser.add_argument('-c', '--channel', action='store', dest='c', help=strings.channel_help)
+parser.add_argument('-a', '--admin', action='store', dest='a', help=strings.admin_help)
 parser.add_argument('-l', '--logs', action='store_true', dest='l', help=strings.logs_help)
 args = parser.parse_args()
 
@@ -87,6 +94,11 @@ if args.l:
 	logs = True
 else:
 	logs = False
+
+if (args.a or args.c) is not None:
+	repost_f = True
+else:
+	repost_f = False
 
 bot = telebot.TeleBot(args.t)
 
@@ -115,10 +127,14 @@ def a(message):
 		bot.send_message(message.chat.id, answer, reply_markup=keyboard)
 	else:
 		answer = random_text(message)
-		if args.c is not None:
+		if repost_f:
 			ikb = types.InlineKeyboardMarkup()
-			button_good = types.InlineKeyboardButton(text=strings.btn_good, callback_data='good')
-			button_bad = types.InlineKeyboardButton(text=strings.btn_bad, callback_data='bad')
+			if message.chat.id == int(args.a):
+				button_good = types.InlineKeyboardButton(text=strings.btn_good, callback_data='good_admin_s')
+				button_bad = types.InlineKeyboardButton(text=strings.btn_bad, callback_data='bad_admin_s')
+			else:
+				button_good = types.InlineKeyboardButton(text=strings.btn_good, callback_data='good')
+				button_bad = types.InlineKeyboardButton(text=strings.btn_bad, callback_data='bad')
 			ikb.add(button_good, button_bad)
 			bot.send_message(message.chat.id, answer, reply_markup=ikb)
 		else:
@@ -127,13 +143,34 @@ def a(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def a(call):
+	# Для пользователей
 	if call.data == 'good':
-		bot.send_message('@' + args.c, call.message.text)
+		ikb = types.InlineKeyboardMarkup()
+		button_good = types.InlineKeyboardButton(text=strings.btn_good, callback_data='good_admin')
+		button_bad = types.InlineKeyboardButton(text=strings.btn_bad, callback_data='bad_admin')
+		ikb.add(button_good, button_bad)
+		bot.send_message(int(args.a), strings.admin_req_hdr + call.message.text, reply_markup=ikb)
 		bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-							  text=call.message.text + strings.posted, reply_markup=None)
+							  text=call.message.text + strings.post_req + ' @' + args.c, reply_markup=None)
 	if call.data == 'bad':
 		bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
 							  text=call.message.text, reply_markup=None)
+
+	# Для админа
+	if call.data == 'good_admin':
+		rps = call.message.text.replace(strings.admin_req_hdr, '')
+		bot.send_message('@' + args.c, rps)
+		bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+	if call.data == 'bad_admin':
+		bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+
+	if call.data == 'good_admin_s':
+		bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+							  text=call.message.text + strings.post_adm, reply_markup=None)
+	if call.data == 'bad_admin_s':
+		bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+							  text=call.message.text, reply_markup=None)
+
 
 while True:
 	try:
